7057d69404e86654893d9b8ab536b20e648130dc
王迪 <wangdi@wangdis-MBP.lan>
2021-08-21 23:10:19 +0800

    feat: 新增etcd 分布式锁demo

--- golang-etcd-demo/app.go
+++ golang-etcd-demo/app.go
@@ -6,82 +6,93 @@ import (
 	"log"
 	"time"
 
+	"github.com/google/uuid"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
-type DistributedMutex struct {
+type DistributedMutex interface {
+	Run(key string, prefix string, f func()) error
+	Close()
 }
 
-func main() {
+type EtcdMutex struct {
+	Cli       *clientv3.Client
+	GrantTime int
+}
+
+func (e *EtcdMutex) Run(key string, prefix string, f func()) error {
+	resp, err := e.Cli.Grant(context.TODO(), int64(e.GrantTime)) // 50s
+	if err != nil {
+		return err
+	}
+	res, err := e.Cli.Put(context.TODO(), key, "host", clientv3.WithLease((resp.ID)))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		e.Cli.Delete(context.TODO(), key)
+	}()
+	// 判断当前key是否有执行权限
+	resp1, err := e.Cli.Get(context.TODO(), prefix, clientv3.WithPrefix(), clientv3.WithSort(clientv3.SortByKey, clientv3.SortDescend))
+	preKey := ""
+	for _, v := range resp1.Kvs {
+		if v.ModRevision-res.Header.Revision == -1 {
+			preKey = string(v.Key)
+			break
+		}
+	}
+	if preKey != "" {
+		done := make(chan int, 0)
+		go func() {
+			// 监听前一个比自己小的key
+			rch := e.Cli.Watch(context.Background(), preKey)
+			for wresp := range rch {
+				for _, ev := range wresp.Events {
+					// 删除事件
+					if string(ev.Kv.Key) == preKey && ev.Type == 1 {
+						close(done)
+					}
+				}
+			}
+		}()
+		<-done
+	}
+	f()
+	return nil
+}
+
+func (e *EtcdMutex) Close() {
+	e.Cli.Close()
+}
+
+func NewEtcdMutex(host string, timeout time.Duration, grantTime int) DistributedMutex {
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"http://127.0.0.1:2379"},
+		Endpoints:   []string{host},
 		DialTimeout: 2 * time.Second,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer cli.Close()
+	return &EtcdMutex{
+		Cli:       cli,
+		GrantTime: grantTime,
+	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
-	_, err = cli.Put(ctx, "sample_key", "wangdi")
-	cancel()
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx1, cancel1 := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
-	_, err = cli.Get(ctx1, "sample_key")
-	cancel1()
-	if err != nil {
-		log.Fatal(err)
-	}
+}
+
+func main() {
+	etcdMutex := NewEtcdMutex("http://127.0.0.1:2379", 2*time.Second, 50)
+	defer etcdMutex.Close()
 	share := 0
-	simulationConcurrent := func(i int) {
-		key := fmt.Sprintf("/lock/mylock/%d", i)
-		resp, err := cli.Grant(context.TODO(), 50)
-		if err != nil {
-			log.Fatal(err)
-		}
-		res, err := cli.Put(context.TODO(), key, "bar", clientv3.WithLease((resp.ID)))
-		if err != nil {
-			log.Fatal(err)
-		}
-		// 默认应该是升序排列
-		resp1, err := cli.Get(context.TODO(), "/lock/mylock", clientv3.WithPrefix(), clientv3.WithSort(clientv3.SortByKey, clientv3.SortDescend))
-		preKey := ""
-		for _, v := range resp1.Kvs {
-			if v.ModRevision-res.Header.Revision == -1 {
-				preKey = string(v.Key)
-				break
-			}
-		}
-		done := make(chan int, 0)
-		if preKey != "" {
-			go func() {
-				// 监听前一个比自己小的key
-				fmt.Println(preKey)
-				rch := cli.Watch(context.Background(), preKey)
-				for wresp := range rch {
-					for _, ev := range wresp.Events {
-						// 删除事件
-						if string(ev.Kv.Key) == preKey && ev.Type == 1 {
-							close(done)
-						}
-					}
-				}
-			}()
-			<-done
-		}
-		// 获取锁
-		// 启动续期定时任务
-		time.Sleep(2 * time.Second)
-		share++
-		fmt.Println(share)
-		cli.Delete(context.Background(), key)
-		return
-	}
 	// single cycle
 	for i := 0; i < 10; i++ {
-		go simulationConcurrent(i)
+		prefix := "/lock/mylock"
+		key := fmt.Sprintf("%s/%s", prefix, uuid.New().String())
+		go etcdMutex.Run(key, prefix, func() {
+			share++
+			fmt.Println(share)
+			time.Sleep(3 * time.Second)
+		})
 	}
 	time.Sleep(1 * time.Hour)
 }
--- golang-etcd-demo/go.mod
+++ golang-etcd-demo/go.mod
@@ -2,4 +2,7 @@ module etcdtest
 
 go 1.16
 
-require go.etcd.io/etcd/client/v3 v3.5.0
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.0
+)
